4ef76d9392a2ecf5c543c0caa18ca5eda56bd2e7
GopiMacha <gopimacha99@gmail.com>
2025-01-17 13:12:08 -0500

    22

--- tfx-pipeline/build/utils.py
+++ tfx-pipeline/build/utils.py
@@ -1,25 +1,26 @@
 import sys
 import os
-from kfp.compiler import Compiler
+
+# Add the tfx-pipeline directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 from pipelines.example_pipeline import create_pipeline
+from kfp.compiler import Compiler
 
 def compile_pipeline():
     pipeline_root = "gs://sinuous-myth-447220-m2_cloudbuild"
     data_path = "data/sample_data.csv"
 
-    # Compile the pipeline directly
+    # Compile the pipeline
     Compiler().compile(
         pipeline_func=lambda: create_pipeline(pipeline_root=pipeline_root, data_path=data_path),
         package_path="example_pipeline.yaml"
     )
 
-def run_pipeline():
-    # Placeholder for triggering the pipeline execution
-    print("Running pipeline is not implemented in this script.")
-
 if __name__ == "__main__":
+    import sys
     mode = sys.argv[2]  # Get mode from command-line arguments
     if mode == "compile-pipeline":
         compile_pipeline()
-    elif mode == "run-pipeline":
-        run_pipeline()
+    else:
+        print("Unsupported mode. Use 'compile-pipeline'.")
