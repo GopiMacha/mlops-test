4b20a1a857e553367ca0820ec2d6b6f735247cdd
GopiMacha <gopimacha99@gmail.com>
2025-01-20 14:45:14 -0500

    added the base649fghkgjkdgg

--- tfx-pipeline/build/utils.py
+++ tfx-pipeline/build/utils.py
@@ -8,12 +8,24 @@ from example_pipeline import create_pipeline
 def compile_pipeline():
     pipeline_root = "gs://sinuous-myth-447220-m2_cloudbuild"
     data_path = "data/sample_data.csv"
+    yaml_output_path = "example_pipeline.yaml"
+    json_output_path = "example_pipeline.json"
 
     # Compile the KFP pipeline
     Compiler().compile(
         pipeline_func=lambda: create_pipeline(pipeline_root=pipeline_root, data_path=data_path),
-        package_path="example_pipeline.json"
+        package_path=yaml_output_path 
     )
+     print(f"Pipeline successfully compiled to '{yaml_output_path}'.")
+
+    # Step 2: Convert the YAML file to JSON format
+    with open(yaml_output_path, "r") as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+
+    with open(json_output_path, "w") as json_file:
+        json.dump(yaml_data, json_file, indent=2)
+
+    print(f"Pipeline successfully converted to JSON: '{json_output_path}'.")
 
 if __name__ == "__main__":
     mode = sys.argv[1]  # Get mode from command-line arguments
